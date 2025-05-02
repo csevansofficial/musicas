126bc9d91a33f0993eae51aed5d159b3ae9b1553
csevansofficial <csevansofficial@gmail.com>
2025-05-02 01:01:31 -0300

    Correção de link temporários

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
+import { createMetaManager } from 'vue-meta'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.use(createMetaManager())
 app.use(router)
 app.mount('#app') 
