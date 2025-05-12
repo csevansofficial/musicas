bbf3ad9ece027ff3dacb5b6cb1b0727e73c6f425
csevansofficial <csevansofficial@gmail.com>
2025-05-12 16:38:03 +0000

    styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
 
     <main class="flex-1 py-8 flex justify-center items-center">
       <div class="w-full max-w-full sm:max-w-xl md:max-w-2xl lg:max-w-4xl xl:max-w-6xl">
-        <p>Todas as músicas disponíveis para ouvir gratuitamente no meu canal do YouTube:<br/>
+        <p class="bg-white shadow-lg rounded-lg p-4 glass">Todas as músicas disponíveis para ouvir gratuitamente no meu canal do YouTube:<br/>
           <a href="https://www.youtube.com/@csevansofficial/releases" target="_blank">C. S. Evans - Canal Oficial no Youtube</a>
         </p>
         <NuxtPage />
